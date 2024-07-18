1fe75060e0baa322ffaeafe1aabe93ae1522fb6e
cjen1 <cjj39@cam.ac.uk>
2024-07-18 08:07:55 +0100

    Add calibre-web

--- nixos-systems/jasper/calibre.nix
+++ nixos-systems/jasper/calibre.nix
@@ -12,7 +12,9 @@
       environment.PGID=100;
 
       volumes = [
-        "/data/calibre/data:/config"
+        "/data/calibre/calibre-config:/config"
+        "/data/calibre/backups:/backups"
+        "/data/calibre/to-add:/to-add"
       ];
       ports = [
         "8080:8080"
@@ -20,5 +22,26 @@
         "8081:8081"
       ];
     };
+    services."calibre-web".service = {
+      image = "lscr.io/linuxserver/calibre-web:latest";
+      restart = "unless-stopped";
+
+      environment.PUID=1000;
+      environment.PGID=100;
+
+      environment.DOCKER_MODS="linuxserver/mods:universal-calibre";
+
+      volumes = [
+        "/data/calibre/calibre-web-config:/config"
+        "/data/calibre/calibre-config/Library:/books"
+      ];
+
+      ports = [
+        "8083:8083"
+      ];
+    };
   };
+  services.caddy.virtualHosts."calibre-web.jentek.dev".extraConfig = ''
+    reverse_proxy 127.0.0.1:8083
+  '';
 }
