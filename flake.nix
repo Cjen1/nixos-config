24d67b052fc8ed1a6d3d0134d05c3af216227007
cjen1 <cjj39@cam.ac.uk>
2025-01-05 13:33:03 +0000

    Move to unstable

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "nixos config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/24.05";
+    nixpkgs.url = "github:nixos/nixpkgs";
     nixpkgs-unstable.url = "github:nixos/nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
