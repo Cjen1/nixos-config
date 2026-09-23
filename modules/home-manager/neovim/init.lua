1728e393ad0d59d6a29f0fc9947bb876f6945c6d
cjen1-msft <chrisjensen@microsoft.com>
2026-09-23 17:35:22 +0100

    fix warning in nvim

--- modules/home-manager/neovim/init.lua
+++ modules/home-manager/neovim/init.lua
@@ -1,14 +1,12 @@
 
 --- underline errors rather than highlight
-vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = true,
-    ---virtual_text = true,
-    signs = true,
-    update_in_insert = true,
-    float = true,
-  }
-)
+vim.diagnostic.config({
+  underline = true,
+  ---virtual_text = true,
+  signs = true,
+  update_in_insert = true,
+  float = true,
+})
 
 vim.o.termguicolors = true
 vim.o.syntax = 'on'
