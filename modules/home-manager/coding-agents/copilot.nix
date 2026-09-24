d100ad055f0d61364db768b608e6f3b26e9efcdb
cjen1-msft <chrisjensen@microsoft.com>
2026-09-24 13:19:45 +0100

    fix copilot

--- modules/home-manager/coding-agents/copilot.nix
+++ modules/home-manager/coding-agents/copilot.nix
@@ -14,7 +14,7 @@ let
     postBuild = ''
       wrapProgram "$out/bin/copilot" \
         --unset COPILOT_ALLOW_ALL \
-        --add-flags "--experimental --assisted-approval"
+        --add-flags "--experimental"
     '';
   };
   wmuxHook = pkgs.writeShellApplication {
